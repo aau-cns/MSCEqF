5a6e4049d7c1d24cbb0a2ae5a65f56ab7f9ccaa6
Fornasier, Alessandro <alessandro.fornasier@aau.at>
2023-01-13 11:24:01 +0100

    [MAINT] implemented rule of five for system state

--- source/msceqf/system/system.cpp
+++ source/msceqf/system/system.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2023 Alessandro Fornasier, Pieter van Goor.
+// Copyright (C) 2023 Alessandro Fornasier.
 // Control of Networked Systems, University of Klagenfurt, Austria.
 //
 // All rights reserved.
@@ -13,6 +13,38 @@
 
 namespace msceqf
 {
+
+SystemState::SystemState(const SystemState& other) : state_()
+{
+  // Copy state_
+  for (const auto& [key, element] : other.state_)
+  {
+    state_[key] = element->clone();
+  }
+}
+
+SystemState::SystemState(SystemState&& other) noexcept : state_(std::move(other.state_)) {}
+
+SystemState& SystemState::operator=(const SystemState& other)
+{
+  // Copy state_
+  state_.clear();
+  for (const auto& [key, element] : other.state_)
+  {
+    state_[key] = element->clone();
+  }
+
+  return *this;
+}
+
+SystemState& SystemState::operator=(SystemState&& other) noexcept
+{
+  state_ = std::move(other.state_);
+  return *this;
+}
+
+SystemState ::~SystemState() { state_.clear(); }
+
 void SystemState::insertSystemStateElement(std::pair<SystemStateKey, SystemStateElementUniquePtr>&& key_ptr)
 {
   assert(key_ptr.first.valueless_by_exception() == false);
@@ -30,4 +62,30 @@ void SystemState::insertSystemStateElement(
     insertSystemStateElement(std::move(key_ptr));
   }
 }
+
+const SE23& SystemState::T() const
+{
+  return std::static_pointer_cast<ExtendedPoseState>(state_.at(SystemStateElementName::T))->T_;
+}
+
+const Vector6& SystemState::b() const
+{
+  return std::static_pointer_cast<BiasState>(state_.at(SystemStateElementName::b))->b_;
+}
+
+const SE3& SystemState::S() const
+{
+  return std::static_pointer_cast<CameraExtrinsicState>(state_.at(SystemStateElementName::S))->S_;
+}
+
+const In& SystemState::K() const
+{
+  return std::static_pointer_cast<CameraIntrinsicState>(state_.at(SystemStateElementName::K))->K_;
+}
+
+const Vector3& SystemState::f(const uint& feat_id) const
+{
+  return std::static_pointer_cast<FeatureState>(state_.at(feat_id))->f_;
+}
+
 }  // namespace msceqf
